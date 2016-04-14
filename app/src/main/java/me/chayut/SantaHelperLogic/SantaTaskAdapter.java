83942a89a1c7270a263a049ec776203aee40f964
chayut orapinpatipat <chayut_o@hotmail.com>
2016-04-14 15:51:49 +1000

    Task management activity test

---
+++ app/src/main/java/me/chayut/SantaHelperLogic/SantaTaskAdapter.java
@@ -0,0 +1,70 @@
+package me.chayut.SantaHelperLogic;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+import me.chayut.santaslittlehelper.R;
+
+/**
+ * Created by chayut on 14/04/16.
+ */
+public class SantaTaskAdapter extends ArrayAdapter<SantaTask>  {
+
+    private static final String TAG = "SantaTaskAdapter";
+
+    Context mContext;
+    private List<SantaTask> mList;
+
+    public SantaTaskAdapter (Context context, int textViewResourceId,
+                           List<SantaTask> objects) {
+
+        super(context, textViewResourceId, objects);
+
+        this.mContext = context;
+        this.mList = objects;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+
+        View rowView;
+
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+
+
+        SantaTask mTask = mList.get(position);
+
+
+        if (mTask instanceof SantaTaskAppoint) {
+            Log.d(TAG,"SantaTaskAppoint");
+            rowView = inflater.inflate(R.layout.row_task_appoint, parent, false);
+        }
+        else if  (mTask instanceof SantaTaskLocation){
+            Log.d(TAG,"SantaTaskLocation");
+            rowView = inflater.inflate(R.layout.row_task_location, parent, false);
+        }
+        else
+        {
+            rowView = inflater.inflate(R.layout.row_task_appoint, parent, false);
+        }
+
+        //TODO: setup User endpoint interaction
+
+        return rowView;
+    }
+
+    public SantaTask getItem(int position){
+
+        return mList.get(position);
+    }
+}
--- app/src/main/java/me/chayut/santaslittlehelper/TaskActivity.java
+++ app/src/main/java/me/chayut/santaslittlehelper/TaskActivity.java
@@ -1,13 +1,121 @@
 package me.chayut.santaslittlehelper;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import me.chayut.SantaHelperLogic.EndPoint;
+import me.chayut.SantaHelperLogic.EndPointAdapter;
+import me.chayut.SantaHelperLogic.SantaHelperLogic;
+import me.chayut.SantaHelperLogic.SantaTask;
+import me.chayut.SantaHelperLogic.SantaTaskAdapter;
+import me.chayut.SantaHelperLogic.SantaTaskAppoint;
+import me.chayut.SantaHelperLogic.SantaTaskLocation;
 
 public class TaskActivity extends AppCompatActivity {
+
+    private final static String TAG = "TaskActivity";
+
+    SantaService mService;
+    SantaHelperLogic mLogic;
+    boolean mBound = false;
+
+    private ListView lvTasks;
+    private SantaTaskAdapter mAdapter;
+    private ArrayList<SantaTask> list = new ArrayList<SantaTask>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+
+        //Listview setup
+        lvTasks = (ListView) findViewById(R.id.listViewTasks);
     }
+
+    private void ListUpdate(){
+        Log.d(TAG,String.format("List Size: %d", list.size()));
+        mAdapter = new SantaTaskAdapter(this,R.layout.row_task_appoint,list);
+        lvTasks.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();  // Always call the superclass method first
+        Log.d(TAG, "onResume()");
+
+
+        if(mBound){
+            Log.d(TAG,mService.getHello());
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Bind to LocalService
+        Intent intent = new Intent(this, SantaService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Unbind from the service
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
+    }
+
+
+
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            SantaService.LocalBinder binder = (SantaService.LocalBinder) service;
+            mService = binder.getService();
+            mLogic = mService.getSantaLogic();
+            mBound = true;
+
+            UIRefresh();
+
+
+            Log.d(TAG,mService.getHello());
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
+
+
+    private void UIRefresh(){
+        list = mService.getSantaLogic().getTaskList();
+        ListUpdate();
+    }
+
+    public void btnAddTaskOnClick(View view){
+        Log.d(TAG,"btnAddTaskOnClick()");
+        mLogic.addAppointTask(new SantaTaskAppoint());
+        mLogic.addLocationTask(new SantaTaskLocation());
+        UIRefresh();
+    }
+
+
+
 }
